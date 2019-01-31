469fe896cb77808726b182eb0a3d2b87d043ac40
DJones2314 <45198188+DJones2314@users.noreply.github.com>
2019-01-31 09:51:48 +0000

    Add files via upload

---
+++ JavaEETemplate-master/src/main/java/com/qa/persistence/domain/Account.java
@@ -0,0 +1,55 @@
+package com.qa.persistence.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+
+	
+	@Id @GeneratedValue (strategy=GenerationType.AUTO)
+	private Long id;
+	@Column (length = 15)
+	private String firstName;
+	@Column (length = 15)
+	private String secondName;
+	@Column (length = 6)
+	private int accountNumber;
+
+	public Account(String firstName, String secondName, int accountNumber) {
+		this.firstName = firstName;
+		this.secondName = secondName;
+		this.accountNumber = accountNumber;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	
+	
+	
+
+}
